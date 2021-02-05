a1b7d7eb1a6865213dbba4bc6ff9f326026b0f41
Lawrence Lu <datalulu@gmail.com>
2021-02-05 19:54:40 +0000

    1

--- R/wardmap.R
+++ R/wardmap.R
@@ -1,22 +1,21 @@
-#' Ward level shape data in Bradford
+#' Ward level shape file for Bradford
 #'
-#' A dataset containing 12.5% of crime incidents data for Bradford city
-#' between 2011 and 2020.
+#' A dataset containing the ward level geometry for Bradford
 #'
 #' @format A data frame with 30,000 rows and 10 variables:
 #' \describe{
-#'   \item{objectid}{Year of the incident}
-#'   \item{wd20cd}{Month of the incident}
-#'   \item{wd20nm}{The force that provided the data about the crime.}
-#'   \item{wd20nmw}{At present, also the force that provided the data about the crime. This is currently being looked into and is likely to change in the near future.}
-#'   \item{bng_e}{The anonymised coordinates of the crime}
-#'   \item{bng_n}{The anonymised coordinates of the crime}
-#'   \item{long}{Percentage of premises that have access to services above 300Mbit/s}
-#'   \item{lat}{The anonymised Lower Layer Super Output Area}
-#'   \item{shape_are}{Crime types}
-#'   \item{shape_len}{Last outcome reported}
-#'   \item{geometry}{Last outcome reported}
+#'   \item{objectid}{objectid}
+#'   \item{wd20cd}{Ward code}
+#'   \item{wd20nm}{Ward name}
+#'   \item{wd20nmw}{Ward NA}
+#'   \item{bng_e}{Easting}
+#'   \item{bng_n}{Northing}
+#'   \item{long}{longitude}
+#'   \item{lat}{Latitude}
+#'   \item{shape_are}{Shape area}
+#'   \item{shape_len}{Shape length}
+#'   \item{geometry}{geometry data}
 #'
 #' }
-#' @source \url{https://data.police.uk/about/#columns}
+#' @source \url{https://geoportal.statistics.gov.uk/datasets/wards-december-2020-uk-bfc-v2}
 "wardmap"
